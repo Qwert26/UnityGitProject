3078989af6cd2e1e135ddb8b7b35987c0f3eed47
Christian Schuerhoff <Qwert26@gmx.de>
2015-06-09 16:52:17 +0200

    Worked on NetworkManager

--- GithubProject/Assets/Scripts/NetworkManager.cs
+++ GithubProject/Assets/Scripts/NetworkManager.cs
@@ -3,6 +3,7 @@ using System.Collections;
 public class NetworkManager : MonoBehaviour {
 	private const string typeName="UniqueGameName";
 	private const string gameName="RoomName";
+	private HostData[] hostList;
 	private void startServer() {
 		Network.InitializeServer(4,25000,!Network.HavePublicAddress());
 		MasterServer.RegisterHost(typeName,gameName);
@@ -15,6 +16,30 @@ public class NetworkManager : MonoBehaviour {
 			if(GUI.Button(new Rect(100,100,250,100),"Start Server")) {
 				startServer();
 			}
+			if(GUI.Button(new Rect(100,250,250,100),"Refresh Hosts")) {
+				refreshHostList();
+			}
+			if(hostList!=null) {
+				for(int i=0;i<hostList.Length;i++) {
+					if(GUI.Button(new Rect(400,100+(110*i),300,100),hostList[i].gameName)) {
+						joinServer(hostList[i]);
+					}
+				}
+			}
 		}
+	}
+	private void refreshHostList() {
+		MasterServer.RequestHostList(typeName);
+	}
+	void OnMasterServerEvent(MasterServerEvent mse) {
+		if(mse==MasterServerEvent.HostListReceived) {
+			hostList=MasterServer.PollHostList();
+		}
+	}
+	private void joinServer(HostData hostData) {
+		Network.Connect(hostData);
+	}
+	void OnConnectedToServer() {
+		Debug.Log("Server Joined");
 	}
 }
